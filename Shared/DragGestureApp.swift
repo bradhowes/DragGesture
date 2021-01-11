f5c4ad3629b28bf0cf2d128063eec6894aaa426c
Brad Howes <bradhowes@mac.com>
2021-01-11 11:56:24 +0100

    Initial Commit

---
+++ Shared/DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureApp.swift
+//  Shared
+//
+//  Created by Brad Howes on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
